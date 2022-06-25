26fa39c258e31ec54aea618b72ef9c0c7c78adfc
NathanHung369 <nathanhung369@outlook.com>
2022-06-25 13:42:50 -0700

    test commit

--- Scrapper.py
+++ Scrapper.py
@@ -2,7 +2,16 @@ import json
 import pymongo
 import pandas as pd
 import re
+import urllib3
 
-print("hi")
+link = "https://www.cargurus.com/Cars/inventorylisting/viewDetailsFilterViewInventoryListing.action?sourceContext=carGurusHomePageModel&entitySelectingHelper.selectedEntity=d2436&zip=98006"
+http = urllib3.PoolManager()
+
+r = http.request('GET', link, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'})
+datastring = str(r.data, "utf-8")
+prices = re.findall("<span class=\"price\">\n\s*<span>\$(.*)<\/span>", datastring)
+for price in prices:
+    price = price.replace(",", "")
+    print(price)
 
 #Nathan's comment
