58226714245a431726c6fbedb4c6ae1f0eafff52
NathanHung369 <nathanhung369@outlook.com>
2022-06-26 13:02:53 -0700

    Update Scrapper.py
    pull data in json

--- Scrapper.py
+++ Scrapper.py
@@ -3,14 +3,21 @@ import pymongo
 import pandas as pd
 import re
 import urllib3
-
-link = "https://www.cargurus.com/Cars/inventorylisting/viewDetailsFilterViewInventoryListing.action?sourceContext=carGurusHomePageModel&entitySelectingHelper.selectedEntity=d2436&zip=98006"
+link = "https://www.cargurus.com/Cars/inventorylisting/viewDetailsFilterViewInventoryListing.action?zip=98006&showNegotiable=true&sortDir=ASC&sourceContext=carGurusHomePageModel&distance=50&sortType=DEAL_SCORE&entitySelectingHelper.selectedEntity=d2436#resultsPage=2"
+#link = "https://www.cargurus.com/Cars/inventorylisting/viewDetailsFilterViewInventoryListing.action?sourceContext=carGurusHomePageModel&entitySelectingHelper.selectedEntity=d2436&zip=98006#resultsPage=4"
 http = urllib3.PoolManager()
 
 r = http.request('GET', link, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'})
 datastring = str(r.data, "utf-8")
-prices = re.findall("<span class=\"price\">\n\s*<span>\$(.*)<\/span>", datastring)
-for price in prices:
-    price = price.replace(",", "")
-    print(price)
-#Nathan's comment
+
+jsonData = re.findall("\"listings\":(.*),\"criteoListingIds\"", datastring)
+#jsonData = re.findall("\"featuredListings\":(.*),\"criteoListingIds\"", datastring)
+jsonDataObject = json.loads(jsonData[0])
+
+#id makeName carYear mileage price daysOnMarket accidentCount ownerCount
+
+for listing in jsonDataObject:
+    print("id:" + str(listing['id']) + " car year:"+ str(listing['carYear'])  + " mileage:"+ str(listing['mileage']) + " price:"+ str(listing['price']) + " daysOnMarket:"+ str(listing['daysOnMarket']) + " accidentCount:"+ str(listing['accidentCount']) + " ownerCount:"+ str(listing['ownerCount']))
+print(len(jsonDataObject))
+
+#need transmission
