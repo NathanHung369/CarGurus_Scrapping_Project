9573968cf0804b65e97340a8d2456669cb0a8c77
NathanHung369 <nathanhung369@outlook.com>
2022-07-05 20:36:11 -0700

    Scrapes data into two databases, Created Alerts to send emailes on new relevant listings

--- Alerts.py
+++ Alerts.py
@@ -1,6 +1,11 @@
 import smtplib
 from email.message import EmailMessage
-
+import urllib3
+import psycopg2
+import urllib.parse as up
+import pandas as pd
+import json
+from datetime import date
 def email_alert(subject, body, to):
     msg = EmailMessage()
     msg.set_content(body)
@@ -10,7 +15,7 @@ def email_alert(subject, body, to):
 
     user = 'nathan.project.alerts@gmail.com'
     msg['from'] = user
-    password = 'lwycjhhhpclnwywz'
+    password = 'apjqhpveglkigdua'
 
     server = smtplib.SMTP("smtp.gmail.com", 587)
     server.starttls()
@@ -19,4 +24,79 @@ def email_alert(subject, body, to):
     server.quit()
 
 if __name__ == '__main__':
-    email_alert('testSubject', 'testEmailBody', 'nathanhung369@outlook.com')
+    maxPrice = 30000
+    maxMiles = 50000
+    cred = json.load(open('PSQLCredentials.json'))
+    url = up.urlparse(cred[0]['connString'])
+    conn = psycopg2.connect(database=url.path[1:], user=url.username, password=url.password, host=url.hostname, port=url.port )
+    cursor  = conn.cursor()
+
+    cursor.execute("select count(*) from listings where insertdate = current_date")
+    cursor.execute("select * from listings where insertdate = current_date")
+    conn.commit()
+    newListings = cursor.fetchall()
+    df = pd.DataFrame(newListings)
+    numOfNewListings = len(df)
+    
+    #print(df.loc[df[1]==2018])
+    emailBody = f"There are {numOfNewListings} new listings today: \n\n"
+    featuredCount = 0
+    listings = []
+    featured = []
+    listings.append(emailBody)
+    for i in range(0, numOfNewListings):
+        listString = ""
+        id = df[0][i]
+        print(id)
+        year = df[1][i]
+        mileage = df[2][i]
+        price = df[3][i]
+        transmission = df[7][i]
+        color = df[8][i]
+        maker = df[10][i]
+
+        listString += f"\n\t{year} {maker} for ${price:,}"
+        listString += f"\n\t\tDetails:"
+        listString += f"\n\t\t\tMileage: {mileage}"
+        listString += f"\n\t\t\tTransmission: {transmission}"
+        listString += f"\n\t\t\tColor: {color}\n\n"
+        listings.append(listString)
+
+        if mileage < maxMiles and price < maxPrice:
+            listString = ""
+            id = df[0][i]
+            
+            year = df[1][i]
+            mileage = df[2][i]
+            price = df[3][i]
+            transmission = df[7][i]
+            color = df[8][i]
+            maker = df[10][i]
+            if maker == "Subaru":
+                link = "https://www.cargurus.com/Cars/inventorylisting/viewDetailsFilterViewInventoryListing.action?sourceContext=carGurusHomePageModel&entitySelectingHelper.selectedEntity=d2134&zip=98006#listing=" + str(id) 
+            else:
+                link = "https://www.cargurus.com/Cars/inventorylisting/viewDetailsFilterViewInventoryListing.action?zip=98006&showNegotiable=true&sortDir=ASC&sourceContext=carGurusHomePageModel&distance=50&sortType=DEAL_SCORE&entitySelectingHelper.selectedEntity=d2436#listing=" + str(id)
+            listString += f"\n\t{year} {maker} for ${price:,}"
+            listString += f"\n\t\tDetails:"
+            listString += f"\n\t\t\tMileage: {mileage}"
+            listString += f"\n\t\t\tTransmission: {transmission}"
+            listString += f"\n\t\t\tColor: {color}\n\n"
+            listString += f"\n\tHere is the Link:\n\t" + link 
+            featured.append(listString)
+            featuredCount += 1
+        
+    cursor.close()
+    conn.close()
+
+    featuredBody = f"There is {featuredCount} listing(s) that are below ${maxPrice} and {maxMiles} miles.\n"
+    partition = "________________________________________________________________________________________\n"
+    listings.insert(0, featuredBody)
+    listings.insert(1, " ".join(featured))
+    listings.insert(2, partition)
+    emailBody = " ".join(listings)
+   
+    
+    emailSubject = str(date.today()) + " Listing Alerts"
+
+
+    email_alert(emailSubject, emailBody, 'nathanhung369@outlook.com')
--- Scrapper.py
+++ Scrapper.py
@@ -19,77 +19,80 @@ models = ["Toyota 86", "Subaru BRZ"]
 http = urllib3.PoolManager()
 
 #Connect to Database
+
 cred = json.load(open('PSQLCredentials.json'))
-url = up.urlparse(cred[0]['connString'])
-conn = psycopg2.connect(database=url.path[1:], user=url.username, password=url.password, host=url.hostname, port=url.port )
-cursor  = conn.cursor()
-cursor.execute("Delete from DayOptions")
-conn.commit()
-cursor.execute("Delete from DayListings")
-conn.commit()
+for c in range(0, 2):
+    print(c)
 
-today = date.today()
-totalOptions = 0
-totalListings = 0
-#Scrape Data and load in JSON object
-for k in range(0, len(links)):
-
-    r = http.request('GET', links[k], headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'})
-    datastring = str(r.data, "utf-8")
-    jsonData = re.findall("\"listings\":(.*),\"criteoListingIds\"", datastring)
-    jsonDataObject = json.loads(jsonData[0])
-
-    
-
-    
-
-    #Inserts data into PSQL data validation tables
-    for i in range(0, len(jsonDataObject)):
-        listing = jsonDataObject[i]
-        id = str(listing['id'])
-        carYear = testNull('carYear')
-        mileage = testNull('mileage')
-        price = testNull('price')
-        daysOnMarket = testNull('daysOnMarket')
-        accidentCount = testNull('accidentCount')
-        ownerCount = testNull('ownerCount')
-        transmission = testNull('localizedTransmission')
-        ExteriorColor = testNull('normalizedExteriorColor')
-        options = listing['options'] 
-       
-        model = models[k]
-        
-
-        sql = "Insert into DayListings (ListingID, CarYear, Mileage, Price, DaysOnMarket, AccidentCount, OwnerCount, Transmission, ExteriorColor, Model) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-        data = (id, carYear, mileage, price, daysOnMarket, accidentCount, ownerCount, transmission, ExteriorColor, model)
-        cursor.execute(sql, data)
-        conn.commit()
-
-        totalListings +=1 
-        for j in range(0, len(options)):
-            sql = "Insert into DayOptions (ListingID, Option) VALUES (%s, %s)"
-            data = (id, options[j])
-            cursor.execute(sql, data)
-            print(sql, data)
-            conn.commit()
-            totalOptions += 1
-
-    #Inserts new data into db
-    cursor.execute("Insert into Listings (listingid, caryear, mileage, price, daysonmarket, accidentcount, ownercount, transmission, exteriorcolor, listingdate, model, insertDate) Select d.listingid, d.caryear, d.mileage, d.price, d.daysonmarket, d.accidentcount, d.ownercount, d.transmission, d.exteriorcolor, current_date, d.model, current_date from DayListings d left join Listings l on d.ListingID = l.ListingID where l.ListingID is null;")
-    conn.commit()   
-
-    cursor.execute("Insert into Options (OptionID, ListingID, Option) Select d.OptionID, d.ListingID, d.Option from DayOptions d join (Select a.listingid from DayListings a left join Listings b on a.ListingID = b.ListingID where b.ListingID is null) l on d.ListingID = l.ListingID;")
-    conn.commit()
-
-    cursor.execute("update listings set daysonmarket = (select daysonmarket from daylistings where listings.ListingID = daylistings.ListingID) From dayListings where Listings.ListingID = dayListings.ListingID")
-    conn.commit()
+    url = up.urlparse(cred[c]['connString'])
+    conn = psycopg2.connect(database=url.path[1:], user=url.username, password=url.password, host=url.hostname, port=url.port )
+    cursor  = conn.cursor()
     cursor.execute("Delete from DayOptions")
     conn.commit()
     cursor.execute("Delete from DayListings")
     conn.commit()
 
-cursor.execute("update listings set listingdate = current_date-daysonmarket")
-conn.commit()
-cursor .close()
-conn.close()
+    today = date.today()
+    totalOptions = 0
+    totalListings = 0
+    #Scrape Data and load in JSON object
+    for k in range(0, len(links)):
+
+        r = http.request('GET', links[k], headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'})
+        datastring = str(r.data, "utf-8")
+        jsonData = re.findall("\"listings\":(.*),\"criteoListingIds\"", datastring)
+        jsonDataObject = json.loads(jsonData[0])
+
+        
+
+        
+
+        #Inserts data into PSQL data validation tables
+        for i in range(0, len(jsonDataObject)):
+            listing = jsonDataObject[i]
+            id = str(listing['id'])
+            carYear = testNull('carYear')
+            mileage = testNull('mileage')
+            price = testNull('price')
+            daysOnMarket = testNull('daysOnMarket')
+            accidentCount = testNull('accidentCount')
+            ownerCount = testNull('ownerCount')
+            transmission = testNull('localizedTransmission')
+            ExteriorColor = testNull('normalizedExteriorColor')
+            options = listing['options'] 
+        
+            model = models[k]
+            
+
+            sql = "Insert into DayListings (ListingID, CarYear, Mileage, Price, DaysOnMarket, AccidentCount, OwnerCount, Transmission, ExteriorColor, Model) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+            data = (id, carYear, mileage, price, daysOnMarket, accidentCount, ownerCount, transmission, ExteriorColor, model)
+            cursor.execute(sql, data)
+            conn.commit()
+
+            totalListings +=1 
+            for j in range(0, len(options)):
+                sql = "Insert into DayOptions (ListingID, Option) VALUES (%s, %s)"
+                data = (id, options[j])
+                cursor.execute(sql, data)
+                conn.commit()
+                totalOptions += 1
+
+        #Inserts new data into db
+        cursor.execute("Insert into Listings (listingid, caryear, mileage, price, daysonmarket, accidentcount, ownercount, transmission, exteriorcolor, listingdate, model, insertDate) Select d.listingid, d.caryear, d.mileage, d.price, d.daysonmarket, d.accidentcount, d.ownercount, d.transmission, d.exteriorcolor, current_date, d.model, current_date from DayListings d left join Listings l on d.ListingID = l.ListingID where l.ListingID is null;")
+        conn.commit()   
+
+        cursor.execute("Insert into Options (OptionID, ListingID, Option) Select d.OptionID, d.ListingID, d.Option from DayOptions d join (Select a.listingid from DayListings a left join Listings b on a.ListingID = b.ListingID where b.ListingID is null) l on d.ListingID = l.ListingID;")
+        conn.commit()
+
+        cursor.execute("update listings set daysonmarket = (select daysonmarket from daylistings where listings.ListingID = daylistings.ListingID) From dayListings where Listings.ListingID = dayListings.ListingID")
+        conn.commit()
+        cursor.execute("Delete from DayOptions")
+        conn.commit()
+        cursor.execute("Delete from DayListings")
+        conn.commit()
+
+    cursor.execute("update listings set listingdate = current_date-daysonmarket")
+    conn.commit()
+    cursor .close()
+    conn.close()
 print("found "+ str(totalListings)+" listings and "+ str(totalOptions) + " options")
