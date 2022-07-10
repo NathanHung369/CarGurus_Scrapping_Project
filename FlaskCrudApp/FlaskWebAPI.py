6d6d6bbbec0040d2cc40f83dd010a7d339dc721e
NathanHung369 <nathanhung369@outlook.com>
2022-07-09 20:06:12 -0700

    created outline for server web api

--- FlaskCrudApp/FlaskWebAPI.py
+++ FlaskCrudApp/FlaskWebAPI.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
 
 app = Flask(__name__)
-
+#route for index
 @app.route('/')
 def Index():
     return render_template("index.html")
+
+#route for submit
+@app.route('/submit', methods=['POST', 'GET'])
+def submit():
+    if request.method == 'POST':
+        multiply = float(request.form['multiply'])
+        result = multiply*2
+        
+        return redirect(url_for("success", results = result))
+
+
+#refresh with answer
+@app.route('/success/<int:results>')
+def success(results):
+    return render_template('showPrice.html', resultPrice= results)
+
 
 if __name__ == "__main__":
     app.run(debug = True)
