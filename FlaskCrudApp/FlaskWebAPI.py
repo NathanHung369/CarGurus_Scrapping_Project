6b4df599f4c22a66826418038b01b97e4d5fc70c
NathanHung369 <nathanhung369@outlook.com>
2022-07-13 18:43:24 -0700

    Added nav bar with new pages

--- FlaskCrudApp/FlaskWebAPI.py
+++ FlaskCrudApp/FlaskWebAPI.py
@@ -5,6 +5,20 @@ app = Flask(__name__)
 @app.route('/')
 def Index():
     return render_template("index.html")
+#route for Resume
+@app.route('/Resume')
+def Resume():
+    return render_template("Resume.html")
+#route for Linkedin
+@app.route('/linkedIn')
+def linkedIn():
+    return render_template("linkedIn.html")
+#route for Contact
+@app.route('/Contact')
+def Contact():
+    return render_template("Contact.html")
+
+
 
 #route for submit
 @app.route('/submit', methods=['POST', 'GET'])
