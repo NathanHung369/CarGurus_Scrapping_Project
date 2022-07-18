d8599d77a5430e5a7e0d090f5919f868041f17bf
NathanHung369 <nathanhung369@outlook.com>
2022-07-18 16:47:48 -0700

    changed sub page github link

--- FlaskCrudApp/FlaskWebAPI.py
+++ FlaskCrudApp/FlaskWebAPI.py
@@ -1,4 +1,4 @@
-from asyncio.windows_events import NULL
+
 from flask import Flask, render_template, url_for, request, redirect
 import os
 import json
@@ -30,7 +30,7 @@ def Design():
 @app.route('/Project/Dashboard')
 def Dashboard():
     return render_template("dashboard.html")
-    
+
 #route for Jupyter Notebook
 @app.route('/Project/Jupyter')
 def Jupyter():
@@ -41,10 +41,12 @@ def Jupyter():
 @app.route('/Resume')
 def Resume():
     return render_template("Resume.html")
+
 #route for Linkedin
-@app.route('/linkedIn')
-def linkedIn():
+@app.route('/github')
+def github():
     return render_template("linkedIn.html")
+
 #route for Contact
 @app.route('/Contact')
 def Contact():
@@ -59,9 +61,10 @@ def submit():
     #Connect to Database and convert relevent data into a DataFrame
     #Connect to Database
     parentDir = os.path.dirname(os.getcwd())
-    print(parentDir)
+    #print(parentDir)
     path = parentDir+'\PSQLCredentials.json'
     #path = parentDir+'/trader33drakor/PSQLCredentials.json'
+    #path = ("~/FlaskCrudApp/PSQLCredentials.json")
     cred = json.load(open(path))
     url = up.urlparse(cred[1]['connString'])
     conn = psycopg2.connect(database=url.path[1:], user=url.username, password=url.password, host=url.hostname, port=url.port )
@@ -77,7 +80,7 @@ def submit():
     if request.method == 'POST':
         year = float(request.form['Year'])#searches form with Name
         mileage = float(request.form['Mileage'])
-        maker = NULL
+        maker = 0
         if(str(request.form['Maker'])== 'Toyota'):
             maker = float(0)
         else:
@@ -91,7 +94,7 @@ def submit():
 #refresh with answer
 @app.route('/Project/Estimator/success/<price>, <year>, <mileage>, <maker>')
 def success(price, year, mileage, maker):
-    return render_template('showPrice.html', price = price, year = str(year), mileage = mileage, maker = maker)
+    return render_template('ShowPrice.html', price = price, year = str(year), mileage = mileage, maker = maker)
 
 
 if __name__ == "__main__":
